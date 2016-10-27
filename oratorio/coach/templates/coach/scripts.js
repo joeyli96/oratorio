008ba6c57f5171dbb678e7cfec4977391fa783d6
Logan Girvin <logangirvin@hotmail.com>
2016-10-27 16:30:45 -0700

    Added menu button behavior.

--- oratorio/coach/templates/coach/scripts.js
+++ oratorio/coach/templates/coach/scripts.js
@@ -1,11 +1,37 @@
 function $(ele) {
-	return document.querySelectorAll(ele);
+	return document.querySelector(ele);
+} 
+
+function $$(ele) {
+	return document.querySeletorAll(ele);
 }
 
+
 window.addEventListener("load", function(){
-    $('#nav-icon3 span').forEach(function(ele) {
-    	ele.addEventListener('click', function(e){
-	        $('#nav-icon3')[0].classList.toggle('open');
-	    });
-    });
+    $('#MenuButton').addEventListener('click', function(e){
+	        $('#MenuButton').classList.toggle('open');
+			$("#Sidebar").classList.toggle('open');
+	});
+
+	window.addEventListener("resize", resize);
+	resize();
 });
+
+function resize(e) {
+	var button = $("#MainButton");
+	var w = window.innerWidth;
+	var h = window.innerHeight;
+	var s = (w > h ? h : w);
+	button.style.width = s * 2 / 3 + "px";
+	button.style.height = s * 2 / 3 + "px";
+	button.style.borderWidth = s * 0.015 + "px";
+	button.style.fontSize = s * 1 / 6 + "px";
+	button.style.lineHeight = s * 2 / 3 + "px";
+	var widthMargin = (w - (s * 2 / 3) ) * (1 / 2 - 0.03);
+	var heightMargin = (h - (s * 2 / 3) ) * (1 / 2 - 0.03);
+	button.style.top = Math.round(heightMargin) + "px";
+	button.style.left = Math.round(widthMargin) + "px";
+
+
+	$("#Sidebar").style.height = h + "px";
+}
