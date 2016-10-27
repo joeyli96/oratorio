4d1bfc7f4ab3c85264123f92529ddf5179d9362f
Joey Li <Joey Li>
2016-10-27 14:24:44 -0700

    Renamed application and added support for templates.

---
+++ oratorio/coach/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+def index(request):
+    template = loader.get_template('coach/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
