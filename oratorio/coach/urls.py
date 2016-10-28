5afced2744840c0121eb3ec55309f02cdd163242
Logan Girvin <logangirvin@hotmail.com>
2016-10-28 15:26:39 -0700

    Got static files working!

--- oratorio/coach/urls.py
+++ oratorio/coach/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -7,4 +9,4 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^profile$', views.profile, name='profile'),
     url(r'^result$', views.result, name='result'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
