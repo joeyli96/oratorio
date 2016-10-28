0e9bee7d68eab44abb19f5c3619f819a551813a5
Logan Girvin <logangirvin@hotmail.com>
2016-10-28 15:16:56 -0700

    Moved templates, renamed links on pages.

--- oratorio/coach/urls.py
+++ oratorio/coach/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'coach'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^profile$', views.profile, name='profile'),
+    url(r'^result$', views.result, name='result'),
 ]
--- oratorio/coach/views.py
+++ oratorio/coach/views.py
@@ -6,3 +6,13 @@ def index(request):
     template = loader.get_template('coach/index.html')
     context = {}
     return HttpResponse(template.render(context, request))
+
+def profile(request):
+    template = loader.get_template('coach/profile.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def result(request):
+    template = loader.get_template('coach/results.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
