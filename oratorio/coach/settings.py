00e79f7ed9bc55e87b297655ca54fce6e2779c8b
Hao Hu <hhu94@uw.edu>
2016-10-27 20:24:09 -0700

    Add static dir

--- oratorio/coach/settings.py
+++ oratorio/coach/settings.py
@@ -48,5 +48,5 @@ DATABASES = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, "static")
 
