a80a14025dc0e6e1c33c4b17763b5006ca5fa140
Logan Girvin <logangirvin@hotmail.com>
2016-10-27 17:24:53 -0700

    Functioning Buttons!

--- oratorio/coach/templates/coach/scripts.js
+++ oratorio/coach/templates/coach/scripts.js
@@ -3,9 +3,10 @@ function $(ele) {
 } 
 
 function $$(ele) {
-	return document.querySeletorAll(ele);
+	return document.querySelectorAll(ele);
 }
 
+var recorder;
 
 window.addEventListener("load", function(){
     $('#MenuButton').addEventListener('click', function(e){
@@ -13,25 +14,142 @@ window.addEventListener("load", function(){
 			$("#Sidebar").classList.toggle('open');
 	});
 
+	$('#MainButton').addEventListener("click", buttonToggle);
+	$(".SideButton.left").addEventListener("click", leftToggle);
+	$(".SideButton.right").addEventListener("click", rightToggle);
+
 	window.addEventListener("resize", resize);
 	resize();
 });
 
-function resize(e) {
+
+
+function buttonToggle(e) {
 	var button = $("#MainButton");
+	if (recorder == null) {
+		newRecorder().then(function(record) {
+		recorder = record;
+		recorder.start();
+		});
+	} else {
+		switch (recorder.state) {
+			case "inactive":
+				recorder.start();
+				console.log("Start");
+				break;
+			case "recording":
+				recorder.stop();
+				console.log("Stopped");
+				break;
+			case "paused":
+				recorder.resume();
+				console.log("Recording");
+				break;
+		}
+	}
+}
+
+function leftToggle(e) {
+	var button = $(".SideButton.left");
+	if (recorder != null) {
+		switch (recorder.state) {
+			case "paused":
+				console.log("RESTART");
+				break;
+		}
+	}
+}
+
+function rightToggle(e) {
+	var button = $(".SideButton.right");
+	if (recorder != null) {
+		switch (recorder.state) {
+			case "recording":
+				recorder.pause();
+				break;
+			case "paused":
+				recorder.stop();
+				break;
+		}
+	}
+}
+
+
+function newRecorder() {
+	return new Promise(function(resolve, reject) {
+		navigator.mediaDevices.getUserMedia({audio: true, video: false}).then(function(mediaStream) {
+		var r = new MediaRecorder(mediaStream);
+		var s = $("#MainButton");
+		var left = $(".SideButton.left");
+		var right = $(".SideButton.right");
+		r.addEventListener("pause", function(e) {
+			s.innerHTML = "RESUME";
+			left.classList.remove("hide");
+			left.innerHTML = "RESTART";
+			right.innerHTML = "STOP";
+			right.classList.add("SideRedButton");
+		});
+		r.addEventListener("resume", function(e) {
+			s.innerHTML = "STOP";
+			left.classList.add("hide");
+			right.innerHTML = "PAUSE";
+			right.classList.remove("SideRedButton");
+		});
+		r.addEventListener("start", function(e) {
+			s.innerHTML = "STOP";
+			right.classList.remove("hide");
+			right.classList.remove("SideRedButton");
+			right.innerHTML = "PAUSE";
+		});
+		r.addEventListener("stop", function(e) {
+			s.innerHTML = "RECORD";
+			left.classList.add("hide");
+			right.classList.add("hide");
+		});
+		resolve(r);
+		}).catch(function(err) {
+		reject(err);
+		});
+	});
+}
+
+function resize(e) {
 	var w = window.innerWidth;
 	var h = window.innerHeight;
 	var s = (w > h ? h : w);
-	button.style.width = s * 2 / 3 + "px";
-	button.style.height = s * 2 / 3 + "px";
+
+	// main button 
+	var button = $("#MainButton");
+	var buttonScale = s * 2 / 3;
+	button.style.width = buttonScale + "px";
+	button.style.height = buttonScale + "px";
 	button.style.borderWidth = s * 0.015 + "px";
 	button.style.fontSize = s * 1 / 6 + "px";
-	button.style.lineHeight = s * 2 / 3 + "px";
-	var widthMargin = (w - (s * 2 / 3) ) * (1 / 2 - 0.03);
-	var heightMargin = (h - (s * 2 / 3) ) * (1 / 2 - 0.03);
+	button.style.lineHeight = buttonScale + "px";
+	var widthMargin = (w - buttonScale ) * (1 / 2 - 0.03);
+	var heightMargin = (h - buttonScale ) * (1 / 2 - 0.03);
 	button.style.top = Math.round(heightMargin) + "px";
 	button.style.left = Math.round(widthMargin) + "px";
 
+	// secondary buttons
+	var smallButtonScale = s * 1 / 4;
+	$$(".SideButton").forEach(function(ele) {
+		ele.style.width = smallButtonScale + "px";
+		ele.style.height = smallButtonScale + "px";
+		ele.style.lineHeight = smallButtonScale + "px";
+		ele.style.fontSize = s * 1 / 16 + "px";
+		ele.style.top = Math.round(heightMargin 
+			+ buttonScale - smallButtonScale) + "px";
+	});
+	var leftButton = $(".SideButton.left");
+	var rightButton = $(".SideButton.right");
+	var circleOffset = 0;
+	leftButton.style.left = Math.round(
+		widthMargin - smallButtonScale + circleOffset) + "px";
+	rightButton.style.left = Math.round(
+		widthMargin  + buttonScale - circleOffset) + "px";
+	
 
+	// side bar height
 	$("#Sidebar").style.height = h + "px";
 }
