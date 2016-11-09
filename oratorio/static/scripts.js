915a540626ad5b573ffe3d3444ee6ac42cefab4c
Joey Li <Joey Li>
2016-11-08 20:09:32 -0800

    Added login function to print user information to the console.

--- oratorio/static/scripts.js
+++ oratorio/static/scripts.js
@@ -17,7 +17,16 @@ window.addEventListener("load", function(){
 	resize();
 });
 
-
+function onSignIn(googleUser) {
+	console.log("hi");
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+  var id_token = googleUser.getAuthResponse().id_token;
+  console.log('ID Token: ' + id_token);
+}
 
 function buttonToggle(e) {
 	var button = $("#MainButton");
