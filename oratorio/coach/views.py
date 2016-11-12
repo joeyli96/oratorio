281bb44788035aabd46445ecdc7eeb5295c2dab8
Yina Zhu <yinazhu@cs.washington.edu>
2016-11-11 18:06:05 -0800

    check pre commit hooks

--- oratorio/coach/views.py
+++ oratorio/coach/views.py
@@ -23,12 +23,15 @@ def upload(request):
     filename = fs.save("testfile.wav", file)
     uploaded_file_url = MEDIA_ROOT + "/" + filename
     tempfile.close()
+    # get user Joey
+    # if user does not exist, create user
     users = User.objects.filter(name="Joey")
     if not users:
         user = User(name="Joey", email="joey@joey.com")
         user.save()
     else:
         user = users[0]
+    # create speech
     num_speeches = len(Speech.objects.all())
     speech_name = "speech" + str(num_speeches + 1)
     speech = Speech(user=user, name=speech_name)
@@ -36,6 +39,7 @@ def upload(request):
     recording = Analyzer.create_recording(audio_dir=uploaded_file_url, speech=speech)
     recording.save()
     print json.dumps(recording.transcript)
+    # send transcript and pace to result page
     template = loader.get_template('coach/results.html')
     rec_len = recording.get_recording_length()
     if rec_len != 0:
